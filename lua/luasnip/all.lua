bb0e760b48a59179cde640a944cf143efc7b4aba
amarlin77 <aaronmarlin5272@gmail.com>
2025-07-23 00:26:06 -0400

    added some custom snippets

--- lua/luasnip/all.lua
+++ lua/luasnip/all.lua
@@ -1,6 +1,16 @@
-return { 
+return {
   s({trig = "helloworld", snippetType="snippet", desc="A hello world snippet", wordTrig = true},
     {t("Just a hint of what's to come"),}
+  ),
+  s({trig = "keymap", snippetType="snippet", desc="vim keymap snippet"},
+    fmta(
+      [[
+      vim.keymap.set("<>", "<>", function() <> end, opts)
+      ]],
+      { i(1, "Mode"),
+        i(2, "mapping"),
+        i(3, "function call")}
+    )
   ),
 }
 
